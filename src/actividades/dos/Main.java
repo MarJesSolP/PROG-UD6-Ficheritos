fd13ff2b06f60cc73ac9f829038437532b59987f
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-06-09 13:44:20 +0100

    feat: hecha actividad dos

--- src/actividades/dos/Main.java
+++ src/actividades/dos/Main.java
@@ -1,7 +1,21 @@
 package actividades.dos;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+            FileReader lector = new FileReader("src/ficheros/actividaddos.text");
+            int caracter;
+            while((caracter = lector.read())!= -1){
+                System.out.print((char)caracter + "_" + caracter + ", ");
+            }
+
+
+
 
     }
 }
